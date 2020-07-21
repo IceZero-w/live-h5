7dad18e83a481cadaeb2a847f5450f3bb838b47b
王圣康 <wangshengkang@wangshengkangdeMacBook-Pro.local>
2020-07-21 20:24:05 +0800

    引入video插件

--- src/views/liveIndex.vue
+++ src/views/liveIndex.vue
@@ -1,24 +1,27 @@
 <template>
-	<div class="font-size">直播首页</div>
+	<div>
+		<div id="live-container"></div>
+	</div>
 </template>
 <script>
+import Player from 'xgplayer';
+
 export default {
-	
-}
-</script>
-<template>
-	<div class="font-size"><span>测试</span>直播首页</div>
-</template>
-<script>
-export default {
-	
+	mounted() {
+		const { innerWidth, innerHeight } = window;
+		const player = new Player({
+			id: 'live-container',
+			url: 'http://192.168.74.50:8081/test.mp4',
+			width: innerWidth,
+			height: innerHeight,
+			autoplay: true,
+			definitionActive: 'click'
+		});
+
+		player.emit('resourceReady', [{name: '高清', url: 'url1'}, {name: '超清', url: 'url2'}]);
+	}
 }
 </script>
 <style lang="scss" scoped>
-.font-size {
-	span {
-		font-size: 30px;
-	}
-	font-size: 18px;
-}
+
 </style>
